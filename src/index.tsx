a731d02a1ae656131f6ce831521a8af42e58c602
deekshasharma <deeksha.sharma25@gmail.com>
2020-07-01 12:35:22 -0700

    An example to render Class Component containing state and props in TypeScript

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import QuoteApp from './QuoteApp';
 import * as serviceWorker from './serviceWorker';
 
+const randomQuotes: string[] = [
+    "Before you judge a man, walk a mile in his shoes. After that who cares?... He’s a mile away and you’ve got his shoes!",
+    "Better to remain silent and be thought a fool than to speak out and remove all doubt.",
+    "The best thing about the future is that it comes one day at a time.",
+    "The only mystery in life is why the kamikaze pilots wore helmets.",
+    "Light travels faster than sound. This is why some people appear bright until you hear them speak.",
+    "The difference between stupidity and genius is that genius has its limits"
+]
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <QuoteApp quotes={randomQuotes}/>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
